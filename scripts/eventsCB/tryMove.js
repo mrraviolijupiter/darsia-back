21bac178d42fd93cf8b87cc1f180c706e216220b
Antiloope <rodrigopizarro1234@gmail.com>
2020-01-29 20:23:09 -0300

    Callbacks prepared to receive client messages. Only try_pass has a debug implementation.

--- scripts/eventsCB/tryMove.js
+++ scripts/eventsCB/tryMove.js
@@ -1,3 +1,3 @@
-module.exports = function () {
-
+module.exports = function (payload, startTurnPayload, arena, character) {
+  sails.log.debug('Try move confirmed: ' + payload);
 };
--- scripts/eventsCB/tryPass.js
+++ scripts/eventsCB/tryPass.js
@@ -1,3 +1,9 @@
-module.exports = function () {
-
+module.exports = function (payload, startTurnPayload, arena, character) {
+  if(character.id === arena.charactersList[arena.turn.characterInTurn].id){
+    sails.log.debug('Is your turn. Mm, was it');
+    clearTimeout(arena.turn.timer);
+    arena.nextTurn('pass',arena.turn.characterInTurn+1);
+  }else{
+    sails.log.debug('It´s not your turn, you could not pass the turn');
+  }
 };
