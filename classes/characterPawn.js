1a82fe14a0c566862ec0308b8a58ae98c8ca0b75
Antiloope <rodrigopizarro1234@gmail.com>
2020-02-25 17:22:53 -0300

    Implemented attack logic. (Without kill routine yet)

--- classes/characterPawn.js
+++ classes/characterPawn.js
@@ -50,6 +50,40 @@ class characterPawn{
     });
     return ret;
   }
+  hit(attackObject){
+    let dice = Math.random();
+    let stats = this.getStats();
+    if (dice < stats.evasionRate){
+      return {
+        type: 'miss',
+        damage: 0.0
+      };
+    }
+    this.currentStats.health -= attackObject.damage;
+
+    return attackObject;
+  }
+  attack(attacked){
+    let attackLocation = attacked.pawn.location;
+    let stats = this.getStats();
+    if (stats.attackRange.find(element => element.x + this.location.x === attackLocation.x && element.y + this.location.y === attackLocation.y)){
+      let ret = {
+        damage: stats.damage,
+        type: 'regular'
+      };
+      let dice = Math.random();
+      if (dice < stats.criticalRate){
+        ret.damage *= (1 + stats.criticalMultiplier);
+        ret.type = 'critical';
+      }
+
+      let attackedPawn = new characterPawn();
+      Object.assign(attackedPawn,attacked.pawn);
+      ret = attackedPawn.hit(ret);
+
+      return ret;
+    }
+  }
 }
 
 module.exports = characterPawn;
