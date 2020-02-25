165509999f4dbc90fb2246072e5931b0a9ac8db0
Antiloope <rodrigopizarro1234@gmail.com>
2020-02-25 13:20:23 -0300

    Corrected try_leave using socket.off and leave

--- scripts/eventsCB/tryLeave.js
+++ scripts/eventsCB/tryLeave.js
@@ -1,17 +1,21 @@
 module.exports = function (payload, startTurnPayload, arena, character) {
-  if (character.id === arena.charactersList[arena.turn.characterInTurn].id){
+  if (character.id === arena.charactersList.find(i => i.id === arena.turn.characterInTurn).id){
     arena.nextTurn('pass',arena.turn.characterInTurn+1);
   }
 
   let protocol = require('../../instances/protocol.js');
 
-  // socket.off(protocol.clientMessages.tryMove.eventName,this.charactersList[i].tryMove);
-  // socket.off(protocol.clientMessages.tryAttack.eventName,this.charactersList[i].tryAttack);
-  // socket.off(protocol.clientMessages.tryLeave.eventName,this.charactersList[i].tryLeave);
-  // socket.off(protocol.clientMessages.tryPass.eventName,this.charactersList[i].tryPass);
+  let socket = sails.sockets.get(character.socket);
+  socket.off(protocol.clientMessages.tryMove.eventName,character.tryMove);
+  socket.off(protocol.clientMessages.tryAttack.eventName,character.tryAttack);
+  socket.off(protocol.clientMessages.tryLeave.eventName,character.tryLeave);
+  socket.off(protocol.clientMessages.tryPass.eventName,character.tryPass);
+  socket.off(protocol.clientMessages.matchReadyConfirm.eventName, payload => { protocol.clientMessages.matchReadyConfirm.callback(payload, character, arena);});
+  socket.off(protocol.clientMessages.requestMatchInfo.eventName, payload => { protocol.clientMessages.requestMatchInfo.callback(payload, character, arena);});
 
   // Remove character from list
   arena.charactersList.splice(arena.charactersList.indexOf(character), 1);
+  sails.sockets.leave(socket,arena.getRoomName());
 
   let leavePayload = protocol.serverMessages.leave.payload;
   leavePayload.characterId = character.id;
